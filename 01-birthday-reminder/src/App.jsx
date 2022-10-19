0ebc11b5a926c4ca6a4d72fb494a12e2d7eb0d24
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-10-19 11:19:12 +0800

    implement the logic to set and clear people

--- 01-birthday-reminder/src/App.jsx
+++ 01-birthday-reminder/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
+import data from './data';
+
+import CelebrantList from './CelebrantList';
+
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
-      <h1>Main Application</h1>
+      <div className="card">
+        <h3>{people.length} birthdays today</h3>
+        <CelebrantList />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </div>
     </main>
   );
 }
---
+++ 01-birthday-reminder/src/CelebrantList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CelebrantList = () => {
+  return <div>CelebrantList</div>;
+};
+
+export default CelebrantList;
