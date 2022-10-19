844548b119ed9c541dba4e566f0c9638a9eacf71
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-10-19 12:31:58 +0800

    implement the initial fetching of tours and displaying empty tours component

---
+++ 02-tours/final/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    // While fetching information display the loading component
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+
+      setLoading(false);
+
+      // Demo an empty tours and confirm the conditional rendering is working
+      setTours([]);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  console.log('TEST TOURS', tours);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return <h2>Tours Project Setup</h2>;
+}
+
+export default App;
