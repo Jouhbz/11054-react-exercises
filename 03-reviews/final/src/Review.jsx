6c8c4981af38912436e8dc9367d86a772399a1cf
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-10-26 11:19:46 +0800

    style the Review component

--- 03-reviews/final/src/Review.jsx
+++ 03-reviews/final/src/Review.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import reviews from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return (
-    <h2>Review Component</h2>
-  )
-}
+  const [index, setIndex] = useState(0);
+  const { name, image, job, text } = reviews[index];
 
-export default Review
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h3 className="author">{name}</h3>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">surprise me</button>
+    </article>
+  );
+};
+
+export default Review;
